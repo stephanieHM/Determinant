40b2236fe6552b8f814e1a6ce089e5bd70f17b5c
stephanie <stephanie.hernandez@upq.edu.mx>
2017-11-05 21:05:00 -0600

    It calculates the matrix's determinant of two different ways: using a command and using an algorithm.

--- determinant.py
+++ determinant.py
@@ -2,29 +2,49 @@
 
 import numpy as np
 
-def menor(M,row,column):
+def menor(M,column):
+
 		n=len(M)
 		Menor=np.empty((n-1,n-1))
 
-
-		return Menor
+		for i in range(0,n-1):
+                	for j in range(column): 
+                	        Menor[i,j]=M[i+1][j] 
+				
+        	for i in range(0,n-1):
+                	for j in range(column,n-1):  
+                	        Menor[i,j]=M[i+1,j+1]
+				
+        	return Menor
 
 def determinant(M):
-		
-		return det
+
+		if len(M)==1:
+               		det=M[0,0]
+        	elif len(M)==2:
+                	det=M[0,0]*M[1,1]-M[0,1]*M[1,0]
+        	else:
+                	det=0
+                	column=0
+                	while column<len(M):
+                        	Men=menor(M,column)
+                        	det=det+np.power(-1,column)*M[0,column]*determinant(Men)
+                        	column=column+1
+
+        	return det
 
 
-print("Ingresa el tamano de la matriz")
+print("Choose the matrix's size: ")
 n=input()
-print("El tamano elegido es: ")
+print("Chosen size is:  ")
 print(n)
-print("La matriz aleatoria de tamano n es: ")
+print("Random matrix of size n is: ")
 M=np.random.random((n,n))
 print(M)
 determinante=np.linalg.det(M)
-print("El determinante, segun NUMPY, es: ")
+print("Determinant (NUMPY) is: ")
 print(determinante)
-"""det=determinant(M)
-print("El determinante es: " )
-print(det)"""  
+det=determinant(M)
+print("Calculated determinant is: " )
+print(det)  
 
